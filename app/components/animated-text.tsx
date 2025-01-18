3e9beb453710e3aaf30265bf4f840ee50c597f55
Joel K George <joelk9895@gmail.com>
2025-01-18 11:14:12 +0530

    Final

--- app/components/animated-text.tsx
+++ app/components/animated-text.tsx
@@ -16,7 +16,7 @@ export const AnimatedText = ({ text, delay, className = "" }: Props) => {
       {letters.map((letter, index) => (
         <span
           key={index}
-          className="inline-block opacity-0 animate-[fade-up_0.5s_ease-out_forwards] text-white"
+          className="inline-block opacity-0 animate-[fade-up_0.5s_ease-out_forwards] text-black dark:text-white"
           style={{
             animationDelay: `${delay + index * 0.05}s`,
             animationFillMode: "forwards",
